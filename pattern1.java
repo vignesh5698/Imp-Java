4b8c11d45b228a13dcb5784b6f4c6ab1652ec8fa
Vignesh <vigneshit13@gmail.com>
2018-12-27 22:12:38 +0530

    Hollow square Pattern
    Output :
    *****
    *   *
    *   *
    *   *
    *****

---
+++ pattern1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class pattern1{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter number : ");
+int n=sc.nextInt();
+for(int i=1;i<=n;i++){
+for(int j=1;j<=n;j++){
+if(i==1||j==1||j==n||i==n){
+System.out.print("*");
+}else{
+System.out.print(" ");
+}
+}
+System.out.println();
+}
+}
+}
